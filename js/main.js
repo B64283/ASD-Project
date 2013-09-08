f9bffc9dcc340dac20a7e4f787d9544fd22c69d1
B64283 <mattdarke@fullsail.edu>
2013-09-08 01:51:49 -0700

    form addpage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 //alert("javaScript works");
 // Matthew Darke
 //ASD 1309 Project1
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+ });	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#Form');
+		    	    
+		    
+		myForm.validate({
+			invalidHandler: function(form, validator) {
+			     			     //var html= ' ';
+			     for(var key in validator.submitted){
+			      var label = $(' label[for^=" ' + key +' "]'); 
